401451246da82264719c0acc14069b65bdef6e38
Qi Zhao <qizhao.ucsc@gmail.com>
2017-08-15 21:49:17 -0700

    feature column strategy done

--- ml/module/feature_column_factory.py
+++ ml/module/feature_column_factory.py
@@ -5,8 +5,10 @@ from __future__ import unicode_literals
 
 import collections
 
-from google.protobuf import text_format
 import tensorflow as tf
+
+from google.protobuf import text_format
+from tensorflow.core.framework import types_pb2
 
 import ml.module.proto.feature_column_schema_pb2 as fcs_pb2
 
@@ -24,215 +26,101 @@ def combiner_type_to_string(combiner):
     raise ValueError("unsupported combiner type: %s" % (combiner))
 
 
-def data_type_to_string(data_type):
-  if data_type == fcs_pb2.DATA_TYPE_INT32:
-    return tf.int32
-  elif data_type == fcs_pb2.DATA_TYPE_INT64:
-    return tf.int64
-  elif data_type == fcs_pb2.DATA_TYPE_FLOAT32:
-    return tf.float32
-  elif data_type == fcs_pb2.DATA_TYPE_FLOAT64:
-    return tf.float64
-  elif data_type == fcs_pb2.DATA_TYPE_STRING:
-    return tf.string
-  else:
-    raise ValueError("unsupported data type: %s" % (data_type))
+class FeatureColumnStrategy(object):
 
+  def __init__(self, schema_file):
+    self._schema_file = schema_file
+    self._column_lu = {}
+    self._config_lu = {}
+    self._visited_configs = set()
+    self._included_columns = set()
+    self._schema_proto = None
+    with tf.gfile.GFile(self._schema_file, "r") as f:
+      self._schema_proto = fcs_pb2.FeatureColumnConfigSchema()
+      text_format.Parse(f.read(), self._schema_proto)
+      self._config_lu = dict((c.name, c)
+                             for c in self._schema_proto.column_config)
 
-def feature_column_schema_from_config(schema_pbtxt):
-  """Loads `FeatureColumnSchema` from text proto file.
+  def _get_dependent_configs(self, config):
+    return [self._config_lu[name] for name in config.dependency]
 
-  Reads in the schema file and parse it as an object.
-  Args:
-    schema_pbtxt: (`FeatureColumnSchema`) text proto file.
+  def _get_dependent_columns(self, config):
+    return [self._column_lu[name] for name in config.dependency]
 
-  Returns:
-    `FeatureColumnSchema` object.
-  """
-  with tf.gfile.GFile(schema_pbtxt, "r") as f:
-    schema = fcs_pb2.FeatureColumnSchema()
-    text_format.Parse(f.read(), schema)
-    return schema
+  def _add_feature_columns_for_config(self, config):
+    if config.name in self._visited_configs:
+      return
+    for dependent_config in self._get_dependent_configs(config):
+      self._add_feature_columns_for_config(dependent_config)
 
+    which_column = config.WhichOneof("specific_column")
+    columns = set()
+    if which_column == "real_valued":
+      real_valued = config.real_valued
+      column = tf.contrib.layers.real_valued_column(
+          config.name,
+          dimension=real_valued.dimension,
+          dtype=tf.DType(real_valued.dtype))
+      columns.add(column)
+      if config.include:
+        self._included_columns.add(column.name)
+    elif which_column == "bucketized":
+      raise NotImplementedError(
+          "no implementation for %s column" % which_column)
+    elif which_column == "one_hot":
+      sparse_column = self._get_dependent_columns(config).pop()
+      column = tf.contrib.layers.one_hot_column(sparse_column)
+      columns.add(column)
+      if config.include:
+        self._included_columns.add(column.name)
+    elif which_column == "hash_bucket":
+      hash_bucket = config.hash_bucket
+      column = tf.contrib.layers.sparse_column_with_hash_bucket(
+          config.name,
+          hash_bucket_size=hash_bucket.hash_bucket_size,
+          combiner=combiner_type_to_string(hash_bucket.combiner),
+          dtype=tf.DType(hash_bucket.dtype))
+      columns.add(column)
+      if config.include:
+        self._included_columns.add(column.name)
+    elif which_column == "integerized":
+      integerized = config.integerized
+      column = tf.contrib.layers.sparse_column_with_integerized_feature(
+          config.name,
+          bucket_size=integerized.bucket_size,
+          dtype=tf.DType(integerized.dtype))
+      columns.add(column)
+      if config.include:
+        self._included_columns.add(column.name)
+    elif which_column == "embedding":
+      sparse_column = self._get_dependent_columns(config).pop()
+      embedding = config.embedding
+      column = tf.contrib.layers.embedding_column(
+          sparse_column,
+          dimension=embedding.dimension,
+          combiner=combiner_type_to_string(embedding.combiner))
+      columns.add(column)
+      if config.include:
+        self._included_columns.add(column.name)
+    elif which_column == "shared_embedding":
+      sparse_columns = self._get_dependent_columns(config)
+      shared = config.shared_embedding
+      shared_columns = tf.contrib.layers.contrib.shared_embedding_columns(
+          sparse_columns,
+          dimension=shared.dimension,
+          combiner=combiner_type_to_string(shared.combiner),
+          dtype=tf.DType(shared.dtype))
+      columns.add(shared_columns)
+      if config.include:
+        self._included_columns.add([c.name for c in shared_columns])
 
-def feature_columns_from_file(schema_pbtxt, include_target):
-  """Creates feature columns by loading `FeatureColumnSchema` from config file.
+    self._visited_configs.add(config.name)
+    self._column_lu.update(dict((c.name, c) for c in columns))
 
-  Args:
-    schema_pbtxt: file path of `FeatureColumnSchema` protobuf text.
-    include_target: a boolean indicating whether return feature column for the target feature.
-
-  Raises:
-    AssertionError: when oneof field is not set.
-
-  Returns:
-    a set of `_FeatureColumn`.
-  """
-  schema = feature_column_schema_from_config(schema_pbtxt)
-  # Key is column name and value is *Column defined in
-  # feature_column_schema.proto.
-  config_lu = dict()
-  # Key is column name and value is `_FeatureColumn`.
-  column_lu = dict()
-  # Key is column name and value is `bool` indicating whether the column
-  # should be included in the result.
-  retain_lu = dict()
-  for column in schema.basic_columns:
-    name = column.name
-    retain_lu[name] = column.retain
-    # Skips label feautre column if include_target is false.
-    if name == FEATURE_LABEL_NAME and not include_target:
-      continue
-    which_column = column.WhichOneof("column")
-    assert which_column is not None
-    # Gets `_FeatureColumn` for the oneof fields of `BasicFeatureColumn`.
-    if which_column == "int_fixed":
-      column_lu[name] = _int_fixed_column_handler(column)
-      config_lu[name] = column.int_fixed
-    elif which_column == "int_var":
-      column_lu[name] = _int_var_column_handler(column)
-      config_lu[name] = column.int_var
-    elif which_column == "float_fixed":
-      column_lu[name] = _float_fixed_column_handler(column)
-      config_lu[name] = column.float_fixed
-    elif which_column == "float_var":
-      column_lu[name] = _float_var_column_handler(column)
-      config_lu[name] = column.float_var
-    elif which_column == "integerized_sparse":
-      column_lu[name] = _integerized_sparse_column_handler(column)
-      config_lu[name] = column.integerized_sparse
-    elif which_column == "hash_bucket_sparse":
-      column_lu[name] = _hash_bucket_sparse_column_handler(column)
-      config_lu[name] = column.hash_bucket_sparse
-    else:
-      raise ValueError("unimplemented handler for column: %s" % (which_column))
-
-  # Gets config and `_FeatureColumn` for derived features.
-  for column in schema.derived_columns:
-    name = column.name
-    retain_lu[name] = column.retain
-    which_column = column.WhichOneof("column")
-    assert which_column is not None
-    if which_column == "embedding":
-      config_lu[name] = column.embedding
-      column_lu[name] = _embedding_handler(column, column_lu, config_lu)
-  elif which_column == "bucketized":
-    config_lu[name] = column.bucketized
-    column_lu[name] = _bucketized_handler(column, column_lu, config_lu)
-  elif which_column == "one_hot":
-    config_lu[name] = column.one_hot
-    column_lu[name] = _one_hot_handler(column, column_lu, config_lu)
-
-  result_columns = set()
-  for name, tf_feature_column in column_lu.items():
-    if not retain_lu[name]:
-      continue
-    result_columns.add(tf_feature_column)
-  return set(result_columns)
-
-
-def _int_fixed_column_handler(column):
-  return (tf.contrib.layers.real_valued_column(column_name=column.name,
-    dimension=column.int_fixed.dimension, dtype=tf.int64))
-
-
-  def _int_var_column_handler(column):
-    raise NotImplementedError("Unimplemented handler for column: %s" %
-        (column.name))
-
-
-    def _float_fixed_column_handler(column):
-      return (tf.contrib.layers.real_valued_column(column_name=column.name,
-        dimension=column.float_fixed.dimension, dtype=tf.float32))
-
-
-      def _float_var_column_handler(column):
-        raise NotImplementedError("Unimplemented handler for column: %s" %
-            (column.name))
-
-
-        def _integerized_sparse_column_handler(column):
-          integerized_sparse = column.integerized_sparse
-  combiner_name = combiner_type_to_string(integerized_sparse.combiner)
-  bucket_size = integerized_sparse.bucket_size
-  data_type = data_type_to_string(integerized_sparse.data_type)
-  return tf.contrib.layers.sparse_column_with_integerized_feature(
-      column_name=column.name,
-      bucket_size=bucket_size,
-      combiner=combiner_name,
-      dtype=data_type)
-
-
-  def _hash_bucket_sparse_column_handler(column):
-    sparse_column = column.hash_bucket_sparse
-  return tf.contrib.layers.sparse_column_with_hash_bucket(column_name=column.name, hash_bucket_size=sparse_column.hash_bucket_size, combiner=combiner_type_to_string(sparse_column.combiner), dtype=data_type_to_string(sparse_column.data_type))
-
-
-_NamedConfigColumn = collections.namedtuple(
-    "_NamedConfigColumn", ["column_name", "column", "config"])
-
-
-def _get_depending_columns_or_raise_error(column, column_lu, config_lu):
-  """Returns the depending columns for derived column.
-
-  All depending columns must be present in the column lookup table, otherwise errors are raised.
-
-  Args:
-    column: (`DerivedFeatureColumn) derived feature column.
-    column_lu: (`dict`) maps column name to `_FeatureColumn` instances.
-    config_lu: (`dict`) maps column name to `BasicFeatureColumn` or `DerivedFeatureColumn`.
-
-  Raises:
-    ValueError: if depending columns are empty.
-    KeyError: if any of the depending column is not found in the lookup.
-
-  Returns:
-    a `set` of `_NamedConfigColumn` instances.
-  """
-  if len(column.depending_columns) == 0:
-    raise ValueError("Depending columns can't be empty.")
-  result_columns = list()
-  for name in column.depending_columns:
-    if name not in column_lu:
-      raise KeyError("Depending column %s not found in column lookup" % (name))
-    if name not in config_lu:
-      raise KeyError("Depending column %s not found in config lookup" % (name))
-    tmp_column = column_lu[name]
-    tmp_config = config_lu[name]
-    result_columns.append(_NamedConfigColumn(
-      column_name=name, column=tmp_column, config=tmp_config))
-    return result_columns
-
-
-def _type_from_list(obj, type_list):
-  return any([isinstance(obj, t) for t in type_list])
-
-
-def _bucketized_column_handler(column, column_lu, config_lu):
-  column_config = _get_depending_columns_or_raise_error(
-      column, column_lu)[0]
-  config = column_config.config
-  assert _type_from_list(
-      config, [fcs_pb2.IntFixedLenColumn, fcs_pb2.FloatFixedLenColumn])
-  # boundaries come from the histogram of the source column.
-  boundaries = [b.upper_boundary for b in config.stats.histogram.buckets]
-  return tf.contrib.layers.bucketized_column(source_column=column_config.column, boundaries=boundaries)
-
-
-def _one_hot_column_handler(column, column_lu, conig_lu):
-  column_config = _get_depending_columns_or_raise_error(
-      column, column_lu, config_lu)[0]
-  assert _type_from_list(column_config.config, [
-    fcs_pb2.HashBucketSparseColumn, fcs_pb2.IntegerizedSparseColumn])
-  return tf.contrib.layers.one_hot_column(sparse_id_column=column_config.column)
-
-
-def _embedding_handler(column, column_lu, config_lu):
-  # Gets depending column config.
-  column_config = _get_depending_columns_or_raise_error(
-      column, column_lu, config_lu)[0]
-  assert _type_from_list(column_config.config, [
-    fcs_pb2.HashBucketSparseColumn, fcs_pb2.IntegerizedSparseColumn])
-  # Gets embedding column configuration.
-  embedding = column.embedding
-  return tf.contrib.layers.embedding_column(sparse_id_column=column_config.column, dimension=embedding.dimension, combiner=combiner_type_to_string(embedding.combiner))
+  def get_feature_columns(self, include_target):
+    for config in self._schema_proto.column_config:
+      self._add_feature_columns_for_config(config)
+    skipped_columns = set([] if include_target else [FEATURE_LABEL_NAME])
+    return dict(
+        (c.name, c) for c in self._column_lu.itervalues()
+        if c.name in self._included_columns and c.name not in skipped_columns)
--- ml/module/feature_column_factory_test.py
+++ ml/module/feature_column_factory_test.py
@@ -7,8 +7,8 @@ import os
 import tensorflow as tf
 
 import ml.common.flags as my_flags
-import ml.module.proto.feature_column_schema_pb2 as fcs_pb2
 import ml.module.feature_column_factory as fcf
+import ml.module.proto.feature_column_schema_pb2 as fcs_pb2
 
 FLAGS = my_flags.FLAGS
 
@@ -16,33 +16,58 @@ FLAGS = my_flags.FLAGS
 class FeatureColumnFactoryTest(tf.test.TestCase):
 
   def setUp(self):
-    self.schema_pbtxt = os.path.join(
+    self._schema_pbtxt = os.path.join(
         FLAGS.test_srcdir, "ml/module/testing_data/feature_column_schema.pbtxt")
-    self.int_fixed = tf.contrib.layers.real_valued_column(
-        "int_fixed", dimension=10, dtype=tf.int64)
-    self.label = tf.contrib.layers.real_valued_column(
+    self._factory = fcf.FeatureColumnStrategy(self._schema_pbtxt)
+    self._int32 = tf.contrib.layers.real_valued_column(
+        "int32", dimension=10, dtype=tf.int32)
+    self._label = tf.contrib.layers.real_valued_column(
         fcf.FEATURE_LABEL_NAME, dimension=1, dtype=tf.int64)
-    self.float_fixed = tf.contrib.layers.real_valued_column(
-        "float_fixed", dimension=20, dtype=tf.float32)
-    self.integerized_sparse = tf.contrib.layers.sparse_column_with_integerized_feature(
-        "integerized_sparse", bucket_size=10, combiner="sum", dtype=tf.int32)
-    self.hash_bucket_sparse = tf.contrib.layers.sparse_column_with_hash_bucket(
-        "hash_bucket_sparse", hash_bucket_size=5000, combiner="mean", dtype=tf.int64)
-    self.embedding = tf.contrib.layers.embedding_column(
-        sparse_id_column=self.hash_bucket_sparse, dimension=50, combiner="sqrt")
+    self._float = tf.contrib.layers.real_valued_column(
+        "float", dimension=20, dtype=tf.float32)
+    self._integerized = (
+        tf.contrib.layers.sparse_column_with_integerized_feature(
+            "integerized", bucket_size=10, combiner="sum", dtype=tf.int32))
+    self._hash_bucket = tf.contrib.layers.sparse_column_with_hash_bucket(
+        "hash_bucket", hash_bucket_size=5000, combiner="mean", dtype=tf.int64)
+    self._embedding = tf.contrib.layers.embedding_column(
+        sparse_id_column=self._hash_bucket, dimension=50, combiner="sqrt")
+
+  def _assertEmbeddingColumnEqual(self, column1, column2):
+    column1 = column1._replace(initializer=None)
+    column2 = column2._replace(initializer=None)
+    self.assertEqual(column1, column2)
 
   def testNotIncludeTarget(self):
-    self.feature_columns = fcf.feature_columns_from_file(
-        self.schema_pbtxt, include_target=False)
-    self.assertItemsEqual(self.feature_columns, set(
-        [self.int_fixed, self.float_fixed, self.integerized_sparse, self.hash_bucket_sparse, self.embedding]))
+    feature_columns_dict = self._factory.get_feature_columns(
+        include_target=False)
+
+    self.assertItemsEqual([
+        "int32", "float", "integerized", "hash_bucket", "hash_bucket_embedding"
+    ], feature_columns_dict.keys())
+    self.assertEqual(feature_columns_dict["int32"], self._int32)
+    self.assertEqual(feature_columns_dict["float"], self._float)
+    self.assertEqual(feature_columns_dict["integerized"], self._integerized)
+    self.assertEqual(feature_columns_dict["hash_bucket"], self._hash_bucket)
+    self._assertEmbeddingColumnEqual(
+        feature_columns_dict["hash_bucket_embedding"], self._embedding)
 
   def testIncludeTarget(self):
-    self.feature_columns = fcf.feature_columns_from_file(
-        self.schema_pbtxt, include_target=True)
-    self.maxDiff = None
-    self.assertItemsEqual(self.feature_columns, set(
-        [self.int_fixed, self.label, self.float_fixed, self.integerized_sparse, self.hash_bucket_sparse, self.embedding]))
+    feature_columns_dict = self._factory.get_feature_columns(
+        include_target=True)
+
+    self.assertItemsEqual([
+        "int32", "float", "integerized", "hash_bucket", "label",
+        "hash_bucket_embedding"
+    ], feature_columns_dict.keys())
+    self.assertEqual(feature_columns_dict["int32"], self._int32)
+    self.assertEqual(feature_columns_dict["label"], self._label)
+    self.assertEqual(feature_columns_dict["float"], self._float)
+    self.assertEqual(feature_columns_dict["integerized"], self._integerized)
+    self.assertEqual(feature_columns_dict["hash_bucket"], self._hash_bucket)
+    self._assertEmbeddingColumnEqual(
+        feature_columns_dict["hash_bucket_embedding"], self._embedding)
+
 
 if __name__ == "__main__":
   tf.logging.set_verbosity(tf.logging.INFO)
